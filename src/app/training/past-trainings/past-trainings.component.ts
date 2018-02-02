49e471eb8f3b9f0b9aceeeac2960663c303c6b30
Ayush Goyal <ayush987goyal@gmail.com>
2018-02-02 23:41:02 +0530

    addition data to db for tables

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    AfterViewInit,
+    ViewChild,
+    OnDestroy
+} from '@angular/core';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
@@ -9,9 +16,11 @@ import { TrainingService } from '../training.service';
     templateUrl: './past-trainings.component.html',
     styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit, AfterViewInit {
+export class PastTrainingsComponent
+    implements OnInit, AfterViewInit, OnDestroy {
     displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
     dataSource = new MatTableDataSource<Exercise>();
+    finishedExercisesSubscription: Subscription;
 
     @ViewChild(MatSort) sort: MatSort;
     @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -19,7 +28,12 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
     constructor(private trainingService: TrainingService) {}
 
     ngOnInit() {
-        this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+        this.finishedExercisesSubscription = this.trainingService.finishedExercisesChanged.subscribe(
+            (exercises: Exercise[]) => {
+                this.dataSource.data = exercises;
+            }
+        );
+        this.trainingService.fetchCompletedOrCancelledExercises();
     }
 
     ngAfterViewInit() {
@@ -29,5 +43,9 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
     doFiler(filterValue: string) {
         this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    ngOnDestroy() {
+        this.finishedExercisesSubscription.unsubscribe();
     }
 }
